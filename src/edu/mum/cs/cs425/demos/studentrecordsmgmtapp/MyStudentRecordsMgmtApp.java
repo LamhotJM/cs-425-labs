7532a2cfa9ebeecb480b679ff90f99d0cd4aa3ac
Lamhotjm <lamhotjms@gmail.com>
2020-03-03 17:33:07 -0600

    printHelloWorld

--- src/edu/mum/cs/cs425/demos/studentrecordsmgmtapp/MyStudentRecordsMgmtApp.java
+++ src/edu/mum/cs/cs425/demos/studentrecordsmgmtapp/MyStudentRecordsMgmtApp.java
@@ -3,7 +3,6 @@ package edu.mum.cs.cs425.demos.studentrecordsmgmtapp;
 import edu.mum.cs.cs425.demos.studentrecordsmgmtapp.model.Student;
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -22,9 +21,9 @@ public class MyStudentRecordsMgmtApp {
         System.out.println("A student who gained admission into the University at least 30 years ago");
         getListOfPlatinumAlumniStudents(list);
 
+        System.out.println("Array filter");
+        printHelloWorld(new int[]{5, 7, 35});
     }
-
-
 
     public static void printListOfStudents(List<Student> students) {
         for (Student stu : students) {
@@ -32,12 +31,35 @@ public class MyStudentRecordsMgmtApp {
         }
     }
 
-    public static void getListOfPlatinumAlumniStudents(List<Student> students)
-    {
+    public static void getListOfPlatinumAlumniStudents(List<Student> students) {
         List<Student> result = students.stream()
                 .filter(s -> s.calculateAge(s.getDateOfAdmission(), LocalDate.now()) >= 30)
                 .sorted(Comparator.comparing(Student::getStudentName))
                 .collect(Collectors.toList());
         result.forEach(System.out::println);
+    }
+
+    /*
+     An array of integers and iterates through them, and it prints the text,
+     "Hello", if the integer is a multiple of 5. It prints the text, "World",
+     if the integer is a multiple of 7. And when it encounters an integer
+     that is a multiple of both 5 and 7, it prints the text, "HelloWorld".
+    */
+    public static void printHelloWorld(int[] ints) {
+        if(ints ==  null || ints.length ==0)
+        {
+            return;
+        }
+        for (int val : ints) {
+            if (val % 5 == 0 && val % 7 == 0) {
+                System.out.println("HelloWorld");
+            } else if (val % 5 == 0) {
+                System.out.println("Hello");
+            } else if (val % 7 == 0)
+                System.out.println("World");
+            else {
+                return;
+            }
+        }
     }
 }
